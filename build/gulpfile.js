44cec0e5fe7e6b0d6827e729b862e7fd6b90ea9f
bluekvirus <bluekvirus@gmail.com>
2015-02-20 02:18:09 +0000

    Updated build script. (wip)

---
+++ build/gulpfile.js
@@ -0,0 +1,27 @@
+/**
+ * Gulp build tasks.
+ *
+ * Tasks
+ * -----
+ * 1. style: LESS -> CSS -> /{dist}/css/main.css
+ * 2. tpl: Templates (html) -> wrap into {name: content} -> /{dist}/templates.json
+ * 3. js: JS -> [babel (es6)] -> [browserify (commonjs bundle)] -> /{dist}/app.js & vendor.js
+ * 4. assets: Assets/* -> copy into /{dist}/*
+ * 5. watch: Watching changes in the /app folder and trigger certain task(s)
+ * 
+ * Configure
+ * ---------
+ * 1. [plugins.js] 
+ * 		(settings for the plugins)
+ * 2. base.js 
+ * 		(dist, watch path:hooks, js[es6?plain? + targets + order/target], ignore list, production[minify?gzip?])
+ *
+ * Default
+ * -------
+ * js will always be linted and css will always be autoprefixed and cleaned.
+ * However, to minify and gzip requires setting the production flag to true in configure.
+ * 
+ * 
+ * @author Tim Lauv
+ * @created 2015.02.19
+ */

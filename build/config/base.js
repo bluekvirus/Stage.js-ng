24325b277fc830f2c05ccc1b6343221ecc71e5ec
bluekvirus <bluekvirus@gmail.com>
2015-02-23 20:12:05 +0000

    Refined base build configure file.

--- build/config/base.js
+++ build/config/base.js
@@ -1,31 +1,93 @@
 /**
- * All path are based on the **gulpfile.js** file. [not this configure file]
+ * This is the base configure file for the build process.
+ * (Do NOT modify unless you want to apply to all build configures)
+ * 
+ * Base Path
+ * ---------
+ * All path are based on the project root. [not this configure file]
  *
  * Configure
  * ---------
  * 1. plugins:
- * 	  	settings for the plugins)
- * 2. base: 
- * 		dist, 
+ * 	  	settings for the plugins.
+ * 2. control aspects: 
+ * 		dist path, 
  * 		watch path:hooks, 
  * 		js[es6?plain? + vendor], 
  * 		assets, 
  * 		production[minify?gzip?]
  *
- * - merged into config/default.js
- *
+ * Override (create another configure)
+ * -----------------------------------
+ * Use the following code:
+ * 
+ * ```
+ * 
+ * var extend = require('extend');
+ * var base = require('./base.js');
+ * exports.config = extend(true, base, {
+ * 		//deep extending the base configure...
+ * });
+ * 
+ * ```
  * 
  * @author Tim Lauv
  * @created 2015.02.19
  *
  */
-exports.config = {
-	//gulp plugin configures
-	plugins: {
 
+exports.config = {
+
+	//---------------dist path--------------
+	output: 'public',
+
+	//-----------inified & gzipped?---------
+	production: false,
+	
+	//--------------js targets--------------
+	//[] --> use concat only, 
+	//'.js' --> as commonjs module and use browserify
+	//
+	es6: true, //only applies to commonjs targets)
+	js: {
+		//app.js
+		app: 'app/main.js',
+
+		//vendor.js
+		vendor: []
 	},
 
-	output: '../public',
+	//--------------css targets-------------
+	//[] --> use concat only, 
+	//'.less' - lessc, 
+	//'.scss' - scss (TBI)
+	//
+	css: {
+		//main.css
+		main: 'app/main.less',
 
-	
+		//vendor.css
+		vendor: []
+	},
+
+	//------------templates.json------------
+	templates: ['app/**/*.tpl.html'],
+
+	//----------------assets----------------
+	assets: ['assets/*'],
+
+	//---------gulp plugin configs----------
+	plugins: {
+
+		less: {},
+		autoprefixer: {},
+		cleancss: {},
+		
+		babelify: {}, //es6
+		browserify: {}, //commonjs
+		
+		uglify: {},
+		gzip: {}
+
+	},
 };

3b40551be0c93935c79362b77be1d197dba566d7
vagrant <vagrant@ubuntu-14.04-amd64-vbox>
2015-07-07 19:40:57 +0000

    Refined core build config and style; Refined default build config and style; cleaned up libs with updates;

--- build/config/_base.js
+++ build/config/_base.js
@@ -80,23 +80,18 @@ module.exports = {
 		libs: [
 			'libs/bower_components/jquery/dist/jquery.js',
 			'libs/vendor/jquery-ui/position.js',
-			'libs/bower_components/velocity/velocity.js', //attr animation
-			'libs/bower_components/velocity/velocity.ui.js', //attr animation-seq
-			'libs/bower_components/kendo-ui-core/src/js/kendo.core.js',
-			'libs/bower_components/kendo-ui-core/src/js/kendo.data.js',
-			'libs/bower_components/kendo-ui-core/src/js/kendo.binder.js',
-			'libs/bower_components/kendo-ui-core/src/js/kendo.fx.js', //effects, effect-q
-			'libs/bower_components/kendo-ui-core/src/js/kendo.userevents.js',
-			'libs/bower_components/kendo-ui-core/src/js/kendo.draganddrop.js',
-			'libs/bower_components/kendo-ui-core/src/js/kendo.sortable.js',
+			
+			//+DnD?
+			//+mobile ready e/tap/gestures?
+
 			'libs/bower_components/lodash/lodash.js',
 			'libs/bower_components/eventemitter2/lib/eventemitter2.js',
 			'libs/bower_components/async/lib/async.js',
 			'libs/bower_components/director/build/director.js',
 			'libs/bower_components/i18next/i18next.js',
 			'libs/bower_components/validator-js/validator.js',
-			//------------------------widgets----------------------
-			'libs/bower_components/bootstrap/dist/js/bootstrap.js',
+			//------------------------no widgets-----------------------------------
+			//------------------------no tpl-engine, mvvm, fx/transition plugins---
 		],
 
 		//framework.js
@@ -107,7 +102,8 @@ module.exports = {
 			'libs/vendor/stagejsv2/com-ajax-ws-rtc.js',
 			'libs/vendor/stagejsv2/i18n-i18next.js',
 			'libs/vendor/stagejsv2/validate-validator.js',
-			'libs/vendor/stagejsv2/navigation-director.js',		
+			'libs/vendor/stagejsv2/navigation-director.js',
+			//------------------------no view-engine-------------------------------
 		]
 	},
 
@@ -132,7 +128,6 @@ module.exports = {
 		{
 			'libs/bower_components/modernizr/modernizr.js':'js',
 			'libs/bower_components/fontawesome/fonts/*':'fonts',
-			'libs/bower_components/bootstrap/dist/fonts/*': 'fonts'
 		},
 	],
 
--- build/config/default.js
+++ build/config/default.js
@@ -1,12 +1,19 @@
 /**
- * Extend from the _base configure, support using the Default view engine (Stage.js v2).
+ * Extend from the _base configure, support using:
+ * 1. Default view engine (Stage.js v2 with Handlebars, Kendo-MVVM, [Velocity]).
+ * 2. Twitter Bootstrap
+ * 3. Bootstrap Material Design
  *
  * Important Note
  * --------------
  * This configure file indicates which View Engine you want to use. If you ever decided
  * to create a new configure file without extending this one, do NOT forget to include the 
  * view engine files. The `./_base.js` configure will not have them specified.
- * (see config.javascript.framework.push(...) below.)
+ * (see config.javascript.framework.push(...) below)
+ *
+ * Note that a specific View Engine will also requires a specific TPL Engine and optionally
+ * an FX plugin bundle.
+ * (see config.javascript.libs.push(...) below)
  *
  * @author Tim Lauv
  * @created 2015.04.27
@@ -21,14 +28,21 @@ config.javascript.app = [
 ];
 
 config.javascript.libs.push(
-	//------------------------additional widgets------------------------
+	//-----------------------------widgets-------------------------------
+	'libs/bower_components/bootstrap/dist/js/bootstrap.js',
 	'libs/bower_components/bootstrap-material-design/dist/js/material.js',
-	'libs/bower_components/bootstrap-material-design/dist/js/ripples.js'
+	'libs/bower_components/bootstrap-material-design/dist/js/ripples.js',
+	//-----------------------------tpl engine, mvvm, fx plugins----------
+	'libs/bower_components/handlebars/handlebars.js',
+	'libs/bower_components/kendo-ui-core/src/js/kendo.core.js',
+	'libs/bower_components/kendo-ui-core/src/js/kendo.data.js',
+	'libs/bower_components/kendo-ui-core/src/js/kendo.binder.js',
+	'libs/bower_components/velocity/velocity.js', //$.velocity - animation [optional, see view-engine.js]
+	'libs/bower_components/velocity/velocity.ui.js' //$.velocity.RegisterEffect/RunSequence - animation-seq [optional]
 );
 
 config.javascript.framework.push(
 	//-----------------------specific view engine------------------------
-	'libs/bower_components/handlebars/handlebars.js',
 	'libs/vendor/stagejsv2/template-helpers-hbs.js',
 	'libs/vendor/stagejsv2/view-engine.js'
 );
@@ -38,6 +52,7 @@ config.stylesheet = 'styles/default.less';
 config.templates = ['src/vanilla/**/*.hbs.html'];
 
 config.assets.push({
+	'libs/bower_components/bootstrap/dist/fonts/*': 'fonts',
 	'libs/bower_components/bootstrap-material-design/dist/fonts/*': 'fonts'
 });
 
